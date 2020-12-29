69f8b5bd00f2fe9a057be796176c9a61a31597cc
justacasul <zhengdl98@gmail.com>
2020-12-29 20:08:53 +0800

    Make some js and css in react

---
+++ acknowledgements.md
@@ -0,0 +1,3 @@
+# Followed the following tutorials:
+https://guides.rubyonrails.org/getting_started.html
+https://pamit.medium.com/todo-list-building-a-react-app-with-rails-api-7a3027907665
---
+++ cvwo-app/src/components/TasksContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class TasksContainer extends Component {
+    render() {
+        return (
+            <div>
+                <div className="listWrapper">
+                    <ol className="taskList">
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TasksContainer
