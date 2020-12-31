f7550593b2d6346bc64edcf1b226967b853ae683
justacasul <zhengdl98@gmail.com>
2020-12-31 17:30:28 +0800

    got the tasks displaying by deleting and re-cloning after npm went haywire

--- cvwo-app/src/components/TasksContainer.js
+++ cvwo-app/src/components/TasksContainer.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class TasksContainer extends Component {
+
+    // default constructor
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+    }
+
+    // use axios to get all tasks
+    getTasks() {
+        axios.get('/tasks')
+            .then(response => {
+                this.setState({tasks: response.data})
+            })
+            .catch(error => console.log(error))
+    }
+
+    // automatically get all tasks
+    componentDidMount() {
+        this.getTasks()
+    }
+
     render() {
         return (
             <div>
                 <div className="listWrapper">
                     <ol className="taskList">
+                        {this.state.tasks.map((task) => {
+                            return(
+                                <li className="task" task={task} key={task.id}>
+                                    <label className="taskLabel">{task.name}</label>
+                                    <span className="deleteTaskBtn">x</span>
+                                </li>
+                            )
+                        })}
                     </ol>
                 </div>
             </div>
